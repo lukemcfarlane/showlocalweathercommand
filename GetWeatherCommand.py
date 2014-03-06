2d9a24852c467a0099c55b8c62c89463ec159ecf
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-03-06 14:23:50 +1300

    Determine and print out wind direction

--- GetWeatherCommand.py
+++ GetWeatherCommand.py
@@ -1,9 +1,18 @@
 import sublime, sublime_plugin, re, urllib, json
 
 class ShowLocalWeatherCommand(sublime_plugin.WindowCommand):
-    def run(window):
+    def getWindDir(self, deg) :
+        val = round((deg - 11.25) / 22.5)
+        arr = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
+        windDirStr = arr[val % 16]
+        return windDirStr
+
+
+    def run(self):
         endpoint = 'http://metservice.com/publicData/oneMinObs_christchurch'
         page_html = urllib.request.urlopen(endpoint).read().decode('utf-8')
-        data = json.loads(page_html)
-        weatherOutput = 'Temp: ' + str(data['temperature']) + ' Wind: ' + str(data['windSpeed']) + '/' + str(data['windGustSpeed'])
+        data = json.loads(page_html) 
+        windDirDeg = data['windDirection']
+        weatherOutput = 'Temp: ' + str(data['temperature']) + ' Wind: ' + str(data['windSpeed']) + '/' + str(data['windGustSpeed']) + ' ' + self.getWindDir(windDirDeg)
         sublime.status_message(weatherOutput)
+
